baeb001f787a264c18a9a1eb06a1c3967f0672a9
Bhushan Jatgade <bhushanjatgade@gmail.com>
2025-09-22 10:22:10 +0530

    Add files via upload

---
+++ javascript-project/Multi-language/englishData.js
@@ -0,0 +1,13 @@
+const englishData = {
+    heading:"PRECISION WELDING",
+    headingtwo:"& METAL FABRICATION",
+    paragraph:"Sai Transformation is Katol based metal manufacturer fabrication company known for roofing sheds, metal and school furniture’s, gates & window grills. A trusted branch of Sai Fabrication & Gas Welding (Est: 1998).",
+    paragraphtwo:" We offer a wide range of metal fabrication services, including Gates,,Window-Grills,Staircases,Industry Shed & Roofing and more. Our team possesses the knowledge and skills to handle any project from standard designs to unique,custom-build structures.",
+    admission_process_heading:`admission-process-heading`,
+    admission_process_list:`
+ <li>Research colleges and courses</li>
+        <li>Fill out application forms</li>
+        <li>Appear for entrance exams</li>
+        <li>Attend counselling/interview sessions</li>
+        <li>Submit required documents and fees</li>`
+}
