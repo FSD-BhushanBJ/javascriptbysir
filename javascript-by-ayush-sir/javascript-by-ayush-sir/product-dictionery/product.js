984714cb02fb295b37933633574a4c28c66b937a
Bhushan Jatgade <bhushanjatgade@gmail.com>
2025-09-22 10:14:51 +0530

    Add files via upload

---
+++ javascript-by-ayush-sir/javascript-by-ayush-sir/product-dictionery/product.js
@@ -0,0 +1,55 @@
+let itProducts = [
+    {
+      ProductName: "Laptop",
+      image: "https://m.media-amazon.com/images/I/61S+iQmF5NL._SX679_.jpg",
+      price: 55000
+    },
+    {
+      ProductName: "Smartphone",
+      image: "https://m.media-amazon.com/images/I/41X9qNxoJKL._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 25000
+    },
+    {
+      ProductName: "Tablet",
+      image: "https://m.media-amazon.com/images/I/71BM3TBipEL._SX679_.jpg",
+      price: 30000
+    },
+    {
+      ProductName: "Desktop PC",
+      image: "https://m.media-amazon.com/images/I/7120H5WUJYL._SX679_.jpg",
+      price: 45000
+    },
+    {
+      ProductName: "Monitor (24 inch)",
+      image: "https://m.media-amazon.com/images/I/410QRmCiZbL._SX300_SY300_QL70_FMwebp_.jpg",
+      price: 15000
+    },
+    {
+      ProductName: "Keyboard",
+      image: "https://m.media-amazon.com/images/I/312gtWap5DL._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 1200
+    },
+    {
+      ProductName: "Mouse",
+      image: "https://m.media-amazon.com/images/I/21+ocjYn8ML._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 800
+    },
+    {
+      ProductName: "Printer",
+      image: "https://m.media-amazon.com/images/I/31u6f4UOYhL._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 18000
+    },
+    {
+      ProductName: "External Hard Drive (1TB)",
+      image: "https://m.media-amazon.com/images/I/21mpp3Wb0bL._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 4500
+    },
+    {
+      ProductName: "Router",
+      image: "https://m.media-amazon.com/images/I/31DtT2LiLdL._SY300_SX300_QL70_FMwebp_.jpg",
+      price: 2500
+    }
+  ];
+  
+  console.log(itProducts);
+  
